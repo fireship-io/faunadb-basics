2baa371f4855521aaf9b37d7bd646a0fc43e7a1d
Jeff Delaney <hello@fireship.io>
2020-10-15 09:46:42 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## FaunaDB Tutorial
+
+Learn the fundamentals of FaunaDB by modeling a twitter-like social graph in Node.js. Code is for reference only - you must create the required collections & indexes in [FaunaDB](https://fauna.com) to run this code. 
+
+Read the full [FaunaDB tutorial](https://fireship.io/lessons/fauna-db-quickstart) or [watch it on YouTube](https://youtu.be/2CipVwISumA). 
+
---
+++ src/index.js
@@ -0,0 +1,104 @@
+const app = require('express')();
+
+const faunadb = require('faunadb');
+const client = new faunadb.Client({ secret: 'YOUR-KEY' })
+
+const {
+    Paginate,
+    Get,
+    Match,
+    Index,
+    Create,
+    Collection,
+    Join,
+    Call,
+    Function: Fn,
+} = faunadb.query;
+
+
+app.get('/tweet/:id', async (req, res) => {
+
+    const doc = await client.query(
+        Get(
+            Ref(
+                Collection('tweets'),
+                req.params.id
+            )
+        )
+    )
+
+    res.send(doc)
+
+});
+
+app.post('/tweet', async (req, res) => {
+
+    const data = {
+        // Original code extracted to Fauna Function
+        // Select('ref', Get(Match(Index('users_by_name'), 'fireship_dev')))
+        user: Call(Fn("getUser"), 'fireship_dev'),
+        text: 'Hola Mundo!'
+    }
+
+    const doc = await client.query(
+        Create(
+            Collection('tweets'),
+            { data }
+        )
+    )
+
+    res.send(doc)
+});
+
+
+app.get('/tweet', async (req, res) => {
+    const docs = await client.query(
+        Paginate(
+            Match(
+                Index('tweets_by_user'),
+                Call(Fn("getUser"), 'fireship_dev')
+            )
+        )
+    )
+
+    res.send(docs)
+});
+
+
+app.post('/relationship', async (req, res) => {
+
+
+    const data = {
+        follower: Call(Fn("getUser"), 'bob'),
+        followee: Call(Fn("getUser"), 'fireship_dev')
+    }
+    const doc = await client.query(
+        Create(
+            Collection('relationships'),
+            { data }
+        )
+    )
+
+    res.send(doc)
+});
+
+
+
+app.get('/feed', async (req, res) => {
+    const docs = await client.query(
+        Paginate(
+            Join(
+                Match(
+                    Index('followees_by_follower'),
+                    Call(Fn("getUser"), 'bob')
+                ),
+                Index('tweets_by_user'),
+            )
+        )
+    )
+
+    res.send(docs)
+});
+
+
+app.listen(5000, () => console.log('API on http://localhost:5000'))
